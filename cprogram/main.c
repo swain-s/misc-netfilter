233b3882eb022e61c4060c64dfe29a36536ed8de
kun shen <shenkun@bupt.edu.cn>
2019-09-11 14:20:35 +0800

    Initial Commit

---
+++ cprogram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cprogram
+//
+//  Created by kun shen on 2019/9/11.
+//  Copyright © 2019 kun shen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
